8e19454400a7f4e01d02eb6de6abba8f517a03d6
DESKTOP-V2O8MM7\okan <okan.tasin@hotmail.com>
2024-11-11 19:21:20 +0000

    push main gateway

--- src/main/java/com/ylncz/apgateway/YlnczApiGatewayApplication.java
+++ src/main/java/com/ylncz/apgateway/YlnczApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.ylncz.apgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,13 @@ public class YlnczApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YlnczApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("user-route", r -> r.path("/api/v1/users/**").uri("lb://user-service"))
+				.build();
 	}
 
 }
