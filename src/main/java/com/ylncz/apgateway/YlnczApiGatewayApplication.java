e2c2aac70dab011fac62bdeee0a51929f1511ed3
DESKTOP-V2O8MM7\okan <okan.tasin@hotmail.com>
2024-11-21 02:36:24 +0300

    fix start

--- src/main/java/com/ylncz/apgateway/YlnczApiGatewayApplication.java
+++ src/main/java/com/ylncz/apgateway/YlnczApiGatewayApplication.java
@@ -3,12 +3,14 @@ package com.ylncz.apgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class YlnczApiGatewayApplication {
 
 	public static void main(String[] args) {
